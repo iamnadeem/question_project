d4a5eed43601590c161a39978cc6e041f4f6d6bb
Nadeem Ahmed <nadeem.a9891@gmail.com>
2023-08-01 23:31:15 +0530

    question project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import data from './data'
+
+
+const App = () => {
+  useState(data)
+  return <h2>Accordion Starter</h2>;
+};
+export default App;
---
+++ src/Questions.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Questions = ({questions}) => {
+  return (
+    <section className='container'>
+        <h1>Questions</h1>
+
+
+    </section>
+
+
+    // <h2>Questions</h2>
+  )
+}
+
+export default Questions
